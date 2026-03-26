04c404e6bb9afdb15fb75530a4c1fa148e7222c7
SushilKhati <50712027+SushilKhati@users.noreply.github.com>
2026-03-26 22:50:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"status": "running"}
-
-@app.get("/products")
-def get_products():
-    return [
-        {"id": 1, "name": "Laptop"},
-        {"id": 2, "name": "Phone"}
-    ]
+def read_root():
+    return {"message": "Hello from FastAPI on Azure!"}
